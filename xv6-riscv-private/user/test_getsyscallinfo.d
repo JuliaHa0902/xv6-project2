2c281f8f648977b87c61f03df7243e5c530496cf
Julia Ha <juliaha0902@gmail.com>
2022-07-09 00:10:26 -0500

    Remove unneccessary file

--- xv6-riscv-private/user/test_getsyscallinfo.d
+++
@@ -1,2 +0,0 @@
-user/test_getsyscallinfo.o: user/test_getsyscallinfo.c kernel/types.h \
- kernel/stat.h user/user.h
--- xv6-riscv-private/user/test_settickets.d
+++
@@ -1,2 +0,0 @@
-user/test_settickets.o: user/test_settickets.c kernel/types.h \
- kernel/stat.h user/user.h
